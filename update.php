7053bbed3432cb3dbc1e720ed120643d3de90605
Captain-G <poppinpites7@gmail.com>
2022-06-24 09:29:10 +0300

    corrected update functionality

--- update.php
+++ update.php
@@ -31,77 +31,96 @@ if (isset($_GET['id'])) {
             $id = $row['id'];
         } 
     ?>
+       <head>
+          <meta charset="UTF-8">
+          <meta http-equiv="X-UA-Compatible" content="IE=edge">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <title>Document</title>
+          <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css">
+          <!-- <link rel="stylesheet" href="form.css"> -->
+        </head>
+        <body style="max-width: 600px; margin:auto">
+        <div class="content my-5" style="font-family: Garamond, serif">
         <h2>User Update Form</h2>
         <form action="" method="post">
           <fieldset>
             <legend>Personal information:</legend>
+            <div class="form-group">
             First name:<br>
-            <input type="text" name="first_name" value="<?php echo $first_name; ?>">
+            <input type="text" name="first_name" class="form-control" value="<?php echo $first_name; ?>">
             <input type="hidden" name="id" value="<?php echo $id; ?>">
-            <br>
+            </div>
+            <div class="form-group">
             Last name:<br>
-            <input type="text" name="last_name" value="<?php echo $last_name; ?>">
-            <br>
+            <input type="text" name="last_name" class="form-control" value="<?php echo $last_name; ?>">
+            </div>
+            <div class="form-group">
             Email:<br>
-            <input type="email" name="email_address" value="<?php echo $email_address; ?>">
-            <br>
+            <input type="email" name="email_address" class="form-control" value="<?php echo $email_address; ?>">
+            </div>
+            <div class="form-group">
             Phone Number:<br>
-            <input type="int" name="phone_number" value="<?php echo $phone_number; ?>">
-            <br>
-            <br><br>
-            <input type="submit" value="Update" name="update">
+            <input type="int" name="phone_number" class="form-control" value="<?php echo $phone_number; ?>">
+            </div>
+            <div class="form-group">
+            <input type="submit" value="Update" name="update" class="btn btn-primary">
+            </div>
           </fieldset>
         </form> 
+      </div>
         </body>
 
+        <!-- <!DOCTYPE html>
+        <html lang="en">
+        <head>
+          <meta charset="UTF-8">
+          <meta http-equiv="X-UA-Compatible" content="IE=edge">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <title>Document</title>
+          <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css">
+          <link rel="stylesheet" href="form.css">
+        </head>
+        <body>
+        <div class="content my-5">
 
 
+        <form action="" method="post" autocomplete="off">
+          <div class="form-group">
+          <label>First name</label>
+          <input type="text" class="form-control" name="first_name" value="<?php echo $first_name; ?>">
+          <input type="hidden" class="form-control" name="id" value="<?php echo $id; ?>">
+          </div>
+          <div class="form-group">
+          <label>Last name</label>
+          <input type="text" class="form-control" name="last_name" value="<?php echo $last_name; ?>">
+          <input type="hidden" class="form-control" name="id" value="<?php echo $id; ?>">
+          </div>
+          <div class="form-group">
+          <label>Email address</label>
+          <input type="text" class="form-control" name="email_address" value="<?php echo $email_address; ?>">
+          <input type="hidden" class="form-control" name="id" value="<?php echo $id; ?>">
+          </div>
+          <div class="form-group">
+          <label>Phone number</label>
+          <input type="text" class="form-control" name="phone_number" value="<?php echo $phone_number; ?>">
+          <input type="hidden" class="form-control" name="id" value="<?php echo $id; ?>">
+          </div>
+      
+      
+          <button type="submit" class="btn btn-primary" name="update" value="Submit">Submit</button>
 
-
-
-        <!-- <!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <!-- Bootstrap CSS -->
-    <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="form.css">
-
-    <title>SignUp</title>
-  </head>
-  <body>
-    <div class="content my-5" style="font-family: Garamond, serif">
-      <h2 style="font-family: Copperplate, Papyrus, fantasy; font-size: larger;">ENTER YOUR DETAILS BELOW</h2>
-
-    <form action="test.php" method="post" autocomplete="off">
-      <div class="form-group">
-        <label for="first_name">First name</label>
-        <input type="text" class="form-control"  name="first_name" value="<?php echo $first_name; ?>"required>
-        <input type="hidden" name="id" value="<?php echo $id;?>">
-      </div>
-      <div class="form-group">
-        <label for="last_name">Last name</label>
-        <input type="text" class="form-control"  name="last_name" value="<?php echo $last_name; ?>">
-        <input type="hidden" name="id" value="<?php echo $id;?>">
-      </div>
-      <div class="form-group">
-        <label for="email">Email address</label>
-        <input type="email" class="form-control"  name="email_address" value="<?php echo $email_address; ?>"required>
-        <input type="hidden" name="id" value="<?php echo $id;?>">
-      </div>
-      <div class="form-group">
-        <label for="first_name">Phone number</label>
-        <input type="text" class="form-control"  name="phone_number" value="<?php echo $phone_number; ?>"required>
-        <input type="hidden" name="id" value="<?php echo $id;?>">
-      </div>
-     
-      <button type="submit" class="btn btn-primary" name="update" value="submit" id="myButton">Submit</button>
+        </div>
 
     </form>
-  </div>  -->
+          
+        </body>
+        </html> -->
 
+        
+
+
+
+       
   
   </body>
 </html> 
